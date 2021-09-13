565d18f7ef1d478d7ed3446e456de66941e9f6e6
Riquelme <matheusriquelme10@hotmail.com>
2021-09-13 12:28:09 -0300

    Commit alterações 13-09-2021

--- App.tsx
+++ App.tsx
@@ -5,6 +5,11 @@ import { Home } from './src/pages/Home';
 
 export default function App() {
   return (
+    //Para executar de npm install
+    //npm start
+    //pod install na pasta IOS
+    //cd ios && arch -x86_64 xed . && cd ..
+    //o xcode ira abrir e la executar
     <>
       <StatusBar 
         barStyle="light-content" 
